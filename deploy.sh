b96a02c5b34cfd7c6537eafde7874d64e27a8737
manikandan <112394806+manikandan171@users.noreply.github.com>
2025-02-05 10:33:24 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
-
-# Print a test message
-echo "hi123"
-
-# Make build.sh executable and run it
-chmod +x build.sh
-./build.sh
-
-# Docker commands
-echo "Logging in to Docker Hub..."
-docker login -u mani1711 -p 'Rithvikmani123#'
-
-# Tag and push the Docker image
-docker tag test mani1711/task-2
-docker push mani1711/task-2
-
-echo "Docker image pushed successfully."
+    echo hi123
+    sh 'chmod +x build.sh'
+    sh './build.sh'
+    docker login -u mani1711 -p Rithvikmani123# 
+    docker tag test mani1711/task-2
+    docker push mani1711/task-2
+    
